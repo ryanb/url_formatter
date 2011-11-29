fdbeeaf6d30675f1ecd24eb351360c91e5bb35c3
Ryan Bates <ryan@railscasts.com>
2011-11-28 18:12:05 -0800

    adding rdoc to format_url method

--- lib/url_formatter/model_additions.rb
+++ lib/url_formatter/model_additions.rb
@@ -1,5 +1,15 @@
 module UrlFormatter
   module ModelAdditions
+    # To format and validate a URL attribute, call <tt>format_url</tt>
+    # in any Active Record model class and pass it the name of an attribute.
+    #
+    #   class User < ActiveRecord::Base
+    #     format_url :website
+    #   end
+    #
+    # This will add a <tt>before_validation</tt> callback to add "http://" to
+    # the attribute if a protocol doesn't exist already. It then validates the
+    # format of the URL.
     def format_url(attribute)
       before_validation do
         send("#{attribute}=", UrlFormatter.format_url(send(attribute)))
