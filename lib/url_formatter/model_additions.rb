7cca3198112e2cca442f426ccbb2648e73424113
Nicolás Hock Isaza <nhocki@gmail.com>
2012-03-27 14:09:57 -0400

    Allow the attribute to be nil without making the model invalid
    For optional values this will make the object invalid because `nil` won't
    match the `UrlFormatter.url_regexp`.

--- lib/url_formatter/model_additions.rb
+++ lib/url_formatter/model_additions.rb
@@ -14,7 +14,7 @@ module UrlFormatter
       before_validation do
         send("#{attribute}=", UrlFormatter.format_url(send(attribute)))
       end
-      validates_format_of attribute, with: UrlFormatter.url_regexp, message: "is not a valid URL"
+      validates_format_of attribute, with: UrlFormatter.url_regexp, message: "is not a valid URL", if: "#{attribute}?"
     end
   end
 end
